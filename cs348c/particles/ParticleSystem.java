441e4f4b35e87c13c0edcaec5f3396aebaabf18f
mjyip8 <madeleinejyip@gmail.com>
2019-02-28 00:26:12 -0800

    First attempt at coding everything up

--- cs348c/particles/ParticleSystem.java
+++ cs348c/particles/ParticleSystem.java
@@ -39,6 +39,14 @@ public class ParticleSystem //implements Serializable
     /** The shader program used by the particles. */
     ShaderProgram prog;
 
+    private double h = Constants.H;
+
+    private double wpoly6_c = 315. / (64. * Math.PI * 64. * Math.pow(h, 9));
+
+    private double wspiky_c = 45./ (Math.PI * Math.pow(h, 6));
+
+    private double inv_rho = 1 / Constants.RHO;
+
 
     /** Basic constructor. */
     public ParticleSystem() {}
@@ -118,6 +126,111 @@ public class ParticleSystem //implements Serializable
         time = 0;
     }
 
+    // refer to slides
+    private double Wpoly6(double r) {
+        if (r >= h) return 0.;
+
+        return wpoly6_c * Math.pow(Math.pow(h, 2) - Math.pow(r, 2), 3);
+    }
+
+    private Vector3d Wspiky(Vector3d r) {
+        if (r.length() >= h) return new Vector3d(0., 0., 0.);
+
+        Vector3d grad = new Vector3d(r);
+        double ratio = wspiky_c * Math.pow(h - r.length(), 2);
+        if (grad.length() != 0.) {
+            grad.scale(1/r.length());
+        }
+        grad.scale(ratio);
+        return grad;
+    }
+
+    //EQN 11
+    private double calcLambda(Particle p) {
+        return -Ci(p) / (sumKGrad(p) + Constants.EPSILON);
+    }
+
+    //EQN 1
+    private double Ci(Particle i) {
+        return (density(i) / Constants.RHO) - 1;
+    }
+
+    //EQN 2
+    private double density(Particle i) {
+        double rho = 0.;
+        Point3d pi = i.x_star;
+
+        for (Particle j : P) {
+            Vector3d pij = new Vector3d(pi);
+            Point3d pj = j.x_star;
+            pij.sub(pj);
+
+            if (pij.length() < h) {
+                rho += (j.m * Wpoly6(pij.length()));
+            }
+        }
+        return rho;
+    }
+
+    //EQN 8
+    private double sumKGrad(Particle i) {
+        Point3d pi = i.x_star;
+        double sum_grad_pk_Ci = 0.;
+        Vector3d sum_pk_grad = new Vector3d(0., 0., 0.);
+
+        for (Particle j : P) {
+            if (j != i) {
+                Vector3d pij = new Vector3d(pi);
+                Point3d pj = j.x_star;
+                pij.sub(pj);
+
+                if (pij.length() < h) { //neighbor check
+                    Vector3d grad_pij = Wspiky(pij);
+                    grad_pij.scale(inv_rho);
+                    sum_pk_grad.add(grad_pij);
+
+                    sum_grad_pk_Ci -= grad_pij.lengthSquared();
+                }
+            }
+        }
+        sum_grad_pk_Ci += sum_pk_grad.lengthSquared();
+        return sum_grad_pk_Ci;
+    }
+
+    //EQN 12
+    private Point3d calcDeltaP(Particle i) {
+        Point3d pi = i.x_star;
+        Point3d dp = new Point3d(0., 0., 0.);
+
+        for (Particle j : P) {
+            if (j != i) {
+                Vector3d pij = new Vector3d(pi);
+                Point3d pj = j.x_star;
+                pij.sub(pj);
+
+                if (pij.length() < h) { //neighbor check
+                    Vector3d grad = Wspiky(pij);
+                    grad.scale(i.lambda + j.lambda);
+                    dp.add(grad);
+                }
+            }
+        }
+        dp.scale(inv_rho);
+        return dp;
+    }
+
+    private double clamp(double val, double min, double max){
+        return Math.max(min, Math.min(max, val));
+    }
+
+    private Point3d enforceBoundaryConstraints(Point3d v) {
+        Point3d new_pos = new Point3d(v);
+        new_pos.x = clamp(new_pos.x, 0., 1.);
+        new_pos.y = clamp(new_pos.y, 0., 1.);
+        new_pos.z = clamp(new_pos.z, 0., 1.);
+        return new_pos;
+    }
+
     /**
      * Simple implementation of a first-order time step. 
      * TODO: Implement the "Position Based Fluids" integrator here
@@ -133,15 +246,42 @@ public class ParticleSystem //implements Serializable
             }
 
             // HACK: GRAVITY (NEED TO USE Force OBJECT)
-            for(Particle p : P)   p.f.y -= p.m * 10.f;
+            for(Particle p : P) {
+                p.f.y -= p.m * 10.f;
+            }
 
         }
 
         /// TIME-STEP: (Symplectic Euler for now):
         for(Particle p : P) {
             p.v.scaleAdd(dt, p.f, p.v); //p.v += dt * p.f;
-            p.x.scaleAdd(dt, p.v, p.x); //p.x += dt * p.v;
+            p.x_star.scaleAdd(dt, p.v, p.x); //p.x += dt * p.v;
+        }
 
+        for (int i = 0; i < 1; i++) {
+            for (Particle p : P) {
+                p.lambda = calcLambda(p);
+            }
+
+            for (Particle p : P) {
+                p.dp = calcDeltaP(p);
+            }
+
+            for (Particle p : P) {
+                p.x_star.add(p.dp);
+                p.x_star = enforceBoundaryConstraints(p.x_star);
+            }
+        }
+
+        for (Particle p : P) {
+            // update v
+            Vector3d new_v = new Vector3d(p.x_star);
+            new_v.sub(p.x);
+            new_v.scale(1 / dt);
+            p.v = new_v;
+
+            //update x
+            p.x = new Point3d(p.x_star);
         }
 
         time += dt;
